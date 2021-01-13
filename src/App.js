12fdd9a169340378ee645ae2903b6ce87e78354c
Arnab <contact.arnab16@gmail.com>
2021-01-14 05:22:16 +0530

    Api Data fetched, UI left

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Card from './Card';
 
 function App() {
+  const url = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=v4tV9pjJksBAGKxc0J8iGTj3Tdlo4MtU"
+  
+  const [results, setResults] = useState([])
+  
+  useEffect(() => {
+      axios.get(url)
+      .then((res) => res.data)
+      .then(data => {
+        setResults(data.results);
+      })
+      .catch(err => console.log("Error", err))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cardContainer">
+      {results.map((result) => {
+        return <Card
+          key = {result.id}
+          url = {result.url}
+          title = {result.title}
+          byline = {result.byline}
+        />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
