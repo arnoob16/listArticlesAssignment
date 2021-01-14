1a4387d152d49268d9d411e465fd8b36be26ecce
Arnab <contact.arnab16@gmail.com>
2021-01-14 07:02:38 +0530

    Added Style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Card from './Card';
+import './App.css';
 
 function App() {
   const url = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=v4tV9pjJksBAGKxc0J8iGTj3Tdlo4MtU"
@@ -22,8 +23,12 @@ function App() {
         return <Card
           key = {result.id}
           url = {result.url}
+          date = {result.published_date}
           title = {result.title}
           byline = {result.byline}
+          imageUrl = {result.media[0]["media-metadata"][2].url}
+          altText = {result.media[0].caption}
+          topic = {result.section}
         />
       })}
     </div>
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const Card = (props) => {
-    console.log(props)
-    return null;
+const Card = ({url, date, title, topic, imageUrl, byline, altText}) => {
+    return (
+        <div className="card">
+            <img src={imageUrl} alt={altText}/>
+            <div className="overlay">
+                <a href={url}><i class="fas fa-external-link-alt"></i>Check News</a>
+            </div>
+            <div className="cardContent">
+                <strong>{title}</strong>
+                <span className="byline">{byline}</span>
+                <span className="date">{date}</span>
+                <span className="topic">{topic}</span>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
