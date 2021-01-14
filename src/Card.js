8dc803670a7976f4c97b83971ec3ef7bd63bb0f0
Arnab <contact.arnab16@gmail.com>
2021-01-14 07:29:17 +0530

    Fixed Lighthouse Performance

--- src/Card.js
+++ src/Card.js
@@ -5,13 +5,15 @@ const Card = ({url, date, title, topic, imageUrl, byline, altText}) => {
         <div className="card">
             <img src={imageUrl} alt={altText}/>
             <div className="overlay">
-                <a href={url}><i class="fas fa-external-link-alt"></i>Check News</a>
+                <a href={url}><i className="fas fa-external-link-alt"></i>Check News</a>
             </div>
             <div className="cardContent">
                 <strong>{title}</strong>
-                <span className="byline">{byline}</span>
-                <span className="date">{date}</span>
-                <span className="topic">{topic}</span>
+                <span className="mutedText">{byline}</span>
+                <div className="cardFooter">
+                    <span className="mutedText">{topic}</span>
+                    <span className="mutedText">{date}</span>
+                </div>
             </div>
         </div>
     );
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            Most Popular Articles from NYTimes
+        </nav>
+    )
+}
+
+export default Navbar;
